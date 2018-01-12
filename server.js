aeaf2f633cd7d7545387d197659596adeccae36a
Kyle Lim <kylelim8998@gmail.com>
2018-01-12 01:56:46 -0500

    Small server change

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
-var io = require('socket.io').listen(server);
-users = [];
-connections = [];
+// var io = require('socket.io').listen(server);
+// users = [];
+// connections = [];
 
 app.use(express.static(__dirname + '/'));
 
@@ -14,45 +14,45 @@ app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
 
-io.sockets.on('connection', function(socket){
-    // Connect Socket
-    connections.push(socket);
-    console.log('Connected: %s sockets connected', connections.length);
+// io.sockets.on('connection', function(socket){
+//     // Connect Socket
+//     connections.push(socket);
+//     console.log('Connected: %s sockets connected', connections.length);
+//
+//     // Disconnect
+//     socket.on('disconnect', function(data){
+//         // if(!socket.username) return;
+//         users.splice(users.indexOf(socket.username), 1);
+//         updateUsernames();
+//         connections.splice(connections.indexOf(socket), 1);
+//         console.log('Disconnected: %s sockets connected', connections.length);
+//     });
+//
+//     // Send Message
+//     socket.on('send message', function(data){
+//         var encodedMsg = data.replace(/</g, "&lt;").replace(/>/g, "&gt;");
+//         // console.log(data);
+//         io.sockets.emit('new message', {msg: encodedMsg, user: socket.username});
+//     });
+//
+//     // New User
+//     socket.on('new user', function(data, callback){
+//         callback(true);
+//         socket.username = data;
+//         users.push(socket.username);
+//         updateUsernames();
+//     });
+//
+//     function updateUsernames(){
+//         io.sockets.emit('get users', users);
+//     }
+// });
 
-    // Disconnect
-    socket.on('disconnect', function(data){
-        // if(!socket.username) return;
-        users.splice(users.indexOf(socket.username), 1);
-        updateUsernames();
-        connections.splice(connections.indexOf(socket), 1);
-        console.log('Disconnected: %s sockets connected', connections.length);
-    });
-
-    // Send Message
-    socket.on('send message', function(data){
-        var encodedMsg = data.replace(/</g, "&lt;").replace(/>/g, "&gt;");
-        // console.log(data);
-        io.sockets.emit('new message', {msg: encodedMsg, user: socket.username});
-    });
-
-    // New User
-    socket.on('new user', function(data, callback){
-        callback(true);
-        socket.username = data;
-        users.push(socket.username);
-        updateUsernames();
-    });
-
-    function updateUsernames(){
-        io.sockets.emit('get users', users);
-    }
-});
-
-const request = require('request')
-
-const apiKey = "AIzaSyB6P2_7QIdwiDtTnX7_BTjAEbXI8nEfGTU"
-const placeId = "ChIJQ2p5wIW2t4kRPRCmMyKuF-Y"
-const url = "https://maps.googleapis.com/maps/api/place/details/json?placeid=" + placeId + "&key=" + apiKey
+// const request = require('request')
+//
+// const apiKey = "AIzaSyB6P2_7QIdwiDtTnX7_BTjAEbXI8nEfGTU"
+// const placeId = "ChIJQ2p5wIW2t4kRPRCmMyKuF-Y"
+// const url = "https://maps.googleapis.com/maps/api/place/details/json?placeid=" + placeId + "&key=" + apiKey
 
 // request.get(url, (err, res, body) => {
 //     if(err){
